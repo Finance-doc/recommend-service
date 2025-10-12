ced4324c642d41f8c556c4ad6545ea4f4431a6ef
minsub <hyohyosubsub@naver.com>
2025-10-12 13:39:47 +0900

    CI/CD Setting change

--- src/main/java/com/financedoc/recommend_service/controller/RecommendController.java
+++ src/main/java/com/financedoc/recommend_service/controller/RecommendController.java
@@ -1,19 +1,19 @@
-package com.financedoc.recommend_service.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/recommend")
-public class RecommendController {
-
-    @GetMapping("/test")
-    public String test(
-            @RequestHeader(value = "X-User-Id") String userId
-    ){
-        return "Recommend Service Test OK :: X-User-Id = "+userId;
-    }
-
-}
+//package com.financedoc.recommend_service.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestHeader;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/recommend")
+//public class RecommendController {
+//
+//    @GetMapping("/test")
+//    public String test(
+//            @RequestHeader(value = "X-User-Id") String userId
+//    ){
+//        return "Recommend Service Test OK :: X-User-Id = "+userId;
+//    }
+//
+//}
